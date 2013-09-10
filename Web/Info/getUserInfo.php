806eb70b080738251d850fe0bb0bf0113c07462d
Brian Freels-Stendel <bfreels@unm.edu>
2013-09-10 10:08:11 -0600

    More LDAP refinement.

--- Web/Info/getUserInfo.php
+++ Web/Info/getUserInfo.php
@@ -3,13 +3,17 @@ define('ROOT_DIR', '../../');
 
 require_once(ROOT_DIR . 'lib/Application/Authentication/namespace.php');
 require_once(ROOT_DIR . 'plugins/Authentication/Ldap/namespace.php');
+require_once ROOT_DIR . 'plugins/Authentication/Ldap/LDAP2.php';
 
 if (isset($_REQUEST["username"]) && $_REQUEST["username"] != "") {
     $username = $_REQUEST["username"];
+    $authorization = new Net_LDAP2();
+ //   $ldap = new Ldap($authorization);
     
-    $userLdap = new Ldap(&$ldap);
+ //   $user_info = $ldap->PopulateUser($username);
+    $user_info = $authorization->getEntry($username);
     
-    $user_info = $userLdap->PopulateUser($username);
+    print var_dump($user_info);
     
    
 }

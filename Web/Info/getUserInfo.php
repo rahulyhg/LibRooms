1666a5206247c8cf5b3c657e83a3263761bb2d53
Brian Freels-Stendel <bfreels@unm.edu>
2013-09-10 14:23:20 -0600

    More LDAP.

--- Web/Info/getUserInfo.php
+++ Web/Info/getUserInfo.php
@@ -7,12 +7,18 @@ require_once ROOT_DIR . 'plugins/Authentication/Ldap/LDAP2.php';
 
 if (isset($_REQUEST["username"]) && $_REQUEST["username"] != "") {
     $username = $_REQUEST["username"];
+    $options = new LdapOptions();
+        
     $authorization = new Net_LDAP2();
- //   $ldap = new Ldap($authorization);
+    
+    $ldap = new Ldap2Wrapper($options);
     
  //   $user_info = $ldap->PopulateUser($username);
-    $user_info = $authorization->getEntry($username);
     
+    $user_info = $ldap->GetUserInfo($username);
+    
+ //   print var_dump(get_defined_vars());
+ //   print "end vars";
     print var_dump($user_info);
     
    

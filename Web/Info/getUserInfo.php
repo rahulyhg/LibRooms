8b4f14e29d40079b40ad28838843c73a11d59a8c
Brian Freels-Stendel <bfreels@unm.edu>
2013-09-11 16:33:21 -0600

    Request for info must come from same server.

--- Web/Info/getUserInfo.php
+++ Web/Info/getUserInfo.php
@@ -1,35 +1,40 @@
 <?php
-define('ROOT_DIR', '../../');
 
-require_once(ROOT_DIR . 'lib/Application/Authentication/namespace.php');
-require_once(ROOT_DIR . 'plugins/Authentication/Ldap/namespace.php');
-require_once ROOT_DIR . 'plugins/Authentication/Ldap/LDAP2.php';
+if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != "" && (substr($_SERVER['HTTP_REFERER'], strpos($_SERVER['HTTP_REFERER'], '//')+2, strpos($_SERVER['HTTP_REFERER'], '/admin')-8) == $_SERVER['SERVER_NAME'])) {
+    
 
-if (isset($_REQUEST["username"]) && $_REQUEST["username"] != "") {
-    $username = $_REQUEST["username"];
-    $options = new LdapOptions();
-        
-    $authorization = new Net_LDAP2();
-    
-    $ldap = new Ldap2Wrapper($options);
-    
-    $ldap->Connect();
-    
-    $ldap->PopulateNonAuthenticatedLDAPUser($username);
-    
-    if ($ldap->GetLdapUser($username) === NULL) {
-        $test = "THERE IS NO USER BY THAT NAME";
-        var_dump($test);
+    define('ROOT_DIR', '../../');
+
+    require_once(ROOT_DIR . 'lib/Application/Authentication/namespace.php');
+    require_once(ROOT_DIR . 'plugins/Authentication/Ldap/namespace.php');
+    require_once ROOT_DIR . 'plugins/Authentication/Ldap/LDAP2.php';
+
+    if (isset($_REQUEST["username"]) && $_REQUEST["username"] != "") {
+        $username = $_REQUEST["username"];
+        $options = new LdapOptions();
+
+        $authorization = new Net_LDAP2();
+
+        $ldap = new Ldap2Wrapper($options);
+
+        $ldap->Connect();
+
+        $ldap->PopulateNonAuthenticatedLDAPUser($username);
+
+        if ($ldap->GetLdapUser($username) === NULL) {
+            $test = "THERE IS NO USER BY THAT NAME";
+            var_dump($test);
+        }
+        else
+        {
+
+            $search_ldap_user = $ldap->GetLdapUser($username);
+
+            $user_data = "addLname|{$search_ldap_user->GetLastName()}||addFname|{$search_ldap_user->GetFirstName()}||addEmail|{$search_ldap_user->GetEmail()}||addPassword|" . strval(rand(10000000,100000000));
+
+            var_dump($user_data);
+        }
+
     }
-    else
-    {
-    
-        $search_ldap_user = $ldap->GetLdapUser($username);
-    
-        $user_data = "addLname|{$search_ldap_user->GetLastName()}||addFname|{$search_ldap_user->GetFirstName()}||addEmail|{$search_ldap_user->GetEmail()}||addPassword|" . strval(rand(10000000,100000000));
-    
-        var_dump($user_data);
-    }
-    
 }
 ?>
